a6b76e515ce2ab61d6a003154a1ccd426596ad2e
Moncher28 <101560889+Moncher28@users.noreply.github.com>
2022-04-19 17:14:16 +1000

    Update DUck.cpp

--- DUck.cpp
+++ DUck.cpp
@@ -2,35 +2,53 @@
 #include <iostream>
 using namespace std;
 
+
+
 class Swim{
 public: virtual void Iswim() = 0;
 };
+
 class Swimming :public Swim{
 public: void Iswim() override { cout << "just swim....nothing"<<endl;}
 };
-class Littleswimming :public Swim{
+
+class Littleswimming :public Swim
+{
 public: void Iswim() override { cout << "Swimming's sounds"<<endl;}
 };
 
-class Quack{
+class Quack
+{
 public: virtual void quack()=0;
 };
-class NormalQuack : public Quack {
-public:void quack() override{
+
+class NormalQuack : public Quack 
+{
+
+public:void quack() override
+{
 cout << "QUACK"<<endl;}
 };
-class LittleQuack : public Quack{
-public:void quack() override {
+
+class LittleQuack : public Quack
+{
+public:void quack() override{
 cout << "quack"<<endl;
 }
 };
 
-class fly{
-public: virtual void Fly() = 0;};
-class Can_fly :public fly {
+class fly
+{
+public: virtual void Fly() = 0;
+};
+
+class Can_fly :public fly
+{
 public:void Fly() override { cout << "I see sky"<<endl;}
 };
-class Never_fly :public fly {
+
+class Never_fly :public fly 
+{
 public:void Fly() override { cout << "I'm never can fly!"<<endl; }
 };
 
@@ -38,20 +56,27 @@ public:void Fly() override { cout << "I'm never can fly!"<<endl; }
 class Displey
 {
 private: string name;
-public: Displey(string name) {
+  
+public: Displey(string name) 
+{
   this->name = name;}
+  
    void Print(){
      cout << name<<"HELLO!"<<endl;}
 };
 
 class Duck
 {
+  
 private:
+  
     Quack &quackStrategy;
     Swim &swimStrategy;
     fly &flyStrategy;
     Displey &displey;
+  
 public:
+  
 Duck(Displey &disp, Quack &quack,Swim &swim, fly &fly) : displey(disp), quackStrategy(quack), swimStrategy(swim),flyStrategy(fly){}
     void Print() {displey.Print();}
     void Quack() { quackStrategy.quack(); }
@@ -67,10 +92,12 @@ void qra(Duck& duck)
     duck.Fly();
 }
  
-  int main(){
+  int main()
+  {
     Duck little_duck = *new Duck(*new Displey("Маленькая утка"), *new     LittleQuack(),*new Swimming(),*new Never_fly());
     Duck Rubber_Duck = *new Duck(*new Displey("Резиновая утка"),*new LittleQuack(), *new Littleswimming(), *new Can_fly());
     qra(little_duck);
     qra(Rubber_Duck);
-    return 0;}
+    return 0;
+  }
  
